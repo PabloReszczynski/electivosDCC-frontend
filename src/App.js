6aa76ae3723178045ebe6b12e65457f66f7649da
Pablo Reszczynski <pablore@me.com>
2017-11-10 12:52:41 -0300

    react-bootstrap

--- src/App.js
+++ src/App.js
@@ -4,14 +4,19 @@ import logo from './logo.svg';
 import './App.css';
 import Electivo from './components/Electivo.jsx';
 import electivos from './electivos2.js';
+import { Clearfix, Col } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {this.props.electivos.electivos.map(electivo => (
-          <Electivo name={electivo.name} comments={electivo.comments} />
-        ))}
+        <Clearfix>
+          {this.props.electivos.electivos.map(electivo => (
+            <Col>
+              <Electivo name={electivo.name} comments={electivo.comments} />
+            </Col>
+          ))}
+        </Clearfix>
       </div>
     );
   }
