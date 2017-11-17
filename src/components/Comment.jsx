2e947b8c3d54e24fb1425ca10e4e9f01e9e5b64b
fabsouto <fabsouto@cl.ibm.com>
2017-11-17 12:25:08 -0300

    Change style for comment and add icons to vote (without functionality)

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,29 +1,36 @@
-import React, { Component } from 'react';
-import { Col, Panel, Glyphicon, Badge } from 'react-bootstrap';
+import React, {Component} from 'react';
+import {Panel, Glyphicon, Badge} from 'react-bootstrap';
 
 const styles = {
-  icon: {
-    padding: '10px'
-  },
-  badge: {
-    margin: '5px'
-  }
-}
+    icon: {
+        margin: '10px 10px 0',
+        cursor: 'pointer',
+    },
+    badge: {
+        margin: '5px'
+    },
+    panel: {
+        margin: '4px 0',
+    },
+    panelText: {
+        boxShadow: '2px 2px 10px -2px rgba(0,0,0,0.75)'
+    }
+};
 
 export default class Comment extends Component {
-  render() {
-    return (
-      <Panel>
-        <Panel>{this.props.txt}</Panel>
-        <div>
-           {/*<Glyphicon glyph="thumbs-up" style={styles.icon}>
-            <Badge style={styles.badge}>{(this.props.votes||{up:0}).up}</Badge>
-          </Glyphicon>
-          <Glyphicon glyph="thumbs-down" style={styles.icon}>
-            <Badge style={styles.badge}>{(this.props.votes||{down:0}).down}</Badge>
-          </Glyphicon>*/}
-        </div>
-      </Panel>
-    )
-  }
+    render() {
+        return (
+            <Panel style={styles.panel}>
+                <Panel style={{...styles.panel, ...styles.panelText}}>{this.props.txt}</Panel>
+                <div>
+                    <Glyphicon glyph="thumbs-up" style={styles.icon}>
+                        <Badge style={styles.badge}>{(this.props.votes || {up: 0}).up}</Badge>
+                    </Glyphicon>
+                    < Glyphicon glyph="thumbs-down" style={styles.icon}>
+                        <Badge style={styles.badge}>{(this.props.votes || {down: 0}).down}</Badge>
+                    </Glyphicon>
+                </div>
+            </Panel>
+        )
+    }
 }
--- src/components/Electivo.jsx
+++ src/components/Electivo.jsx
@@ -1,38 +1,38 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Comment from './Comment.jsx';
-import { Col } from 'react-bootstrap';
 
 const styles = {
-  container: {
-    backgroundColor: 'white',
-    marginBottom: '10px',
-    marginTop: '10px'
-  },
-  comments: {
-    overflow: 'scroll',
-    maxHeight: '500px'
-  },
-  header: {
-    padding: '10px',
-    backgroundColor: '#2196F3',
-    color: 'white'
-  }
-}
+    container: {
+        backgroundColor: 'white',
+        marginBottom: '10px',
+        marginTop: '10px'
+    },
+    comments: {
+        overflow: 'scroll',
+        maxHeight: '500px',
+        backgroundColor: 'rgb(200, 200, 200)'
+    },
+    header: {
+        padding: '10px',
+        backgroundColor: '#2196F3',
+        color: 'white'
+    }
+};
 
 export default class Electivo extends Component {
-  render() {
-      console.log(this.props);
-    return (
-      <div style={styles.container}> 
-        <div style={styles.header}>
-          <h1>{this.props.name}</h1>
-        </div>
-        <div style={styles.comments}>
-          {this.props.comments.map(comment => (
-            <Comment txt={comment.txt} votes={comment.votes} />
-          ))}
-        </div>
-      </div>
-    )
-  }
+    render() {
+        console.log(this.props);
+        return (
+            <div style={styles.container}>
+                <div style={styles.header}>
+                    <h1>{this.props.name}</h1>
+                </div>
+                <div style={styles.comments}>
+                    {this.props.comments.map(comment => (
+                        <Comment txt={comment.txt} votes={comment.votes}/>
+                    ))}
+                </div>
+            </div>
+        )
+    }
 }
